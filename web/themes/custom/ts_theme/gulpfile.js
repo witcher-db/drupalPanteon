90285a69c86f0685579ea6d49481318e4bbe4316
witcher <o.polishchuk@dev-branch.com>
2025-09-10 09:49:10 +0300

    refs #9647: Fixed styling related issues
    Make hero banner responsive. Fixed bugs in styling

--- web/themes/custom/ts_theme/gulpfile.js
+++ web/themes/custom/ts_theme/gulpfile.js
@@ -1,30 +1,29 @@
-let gulp = require('gulp'),
-
-  sass = require('gulp-sass')(require('sass')),
-  sourcemaps = require('gulp-sourcemaps'),
-  cached = require('gulp-cached'),
-  newer = require('gulp-newer'),
-  $ = require('gulp-load-plugins')(),
-  cleanCss = require('gulp-clean-css'),
-  rename = require('gulp-rename'),
-  postcss = require('gulp-postcss'),
-  autoprefixer = require('autoprefixer'),
-  postcssInlineSvg = require('postcss-inline-svg'),
-  browserSync = require('browser-sync').create(),
-  pxtorem = require('postcss-pxtorem'),
+let gulp = require("gulp"),
+  sass = require("gulp-sass")(require("sass")),
+  sourcemaps = require("gulp-sourcemaps"),
+  cached = require("gulp-cached"),
+  newer = require("gulp-newer"),
+  $ = require("gulp-load-plugins")(),
+  cleanCss = require("gulp-clean-css"),
+  rename = require("gulp-rename"),
+  postcss = require("gulp-postcss"),
+  autoprefixer = require("autoprefixer"),
+  postcssInlineSvg = require("postcss-inline-svg"),
+  browserSync = require("browser-sync").create(),
+  pxtorem = require("postcss-pxtorem"),
   postcssProcessors = [
     postcssInlineSvg({
       removeFill: true,
-      paths: ['./node_modules/bootstrap-icons/icons'],
+      paths: ["./node_modules/bootstrap-icons/icons"],
     }),
     pxtorem({
       propList: [
-        'font',
-        'font-size',
-        'line-height',
-        'letter-spacing',
-        '*margin*',
-        '*padding*',
+        "font",
+        "font-size",
+        "line-height",
+        "letter-spacing",
+        "*margin*",
+        "*padding*",
       ],
       mediaQuery: true,
     }),
@@ -32,32 +31,34 @@ let gulp = require('gulp'),
 
 const paths = {
   scss: {
-    src: './src/scss/style.scss',
-    dest: './dest/css',
-    watch: './src/scss/**/*.scss',
-    bootstrap: './node_modules/bootstrap/scss/bootstrap.scss',
+    src: "./src/scss/style.scss",
+    dest: "./dest/css",
+    watch: "./src/scss/**/*.scss",
+    bootstrap: "./node_modules/bootstrap/scss/bootstrap.scss",
   },
   js: {
-    bootstrap: './node_modules/bootstrap/dist/js/bootstrap.min.js',
-    popper: './node_modules/@popperjs/core/dist/umd/popper.min.js',
-    base: '../../contrib/bootstrap/js/base.js',
-    src: './src/js/custom.js',
-    dest: './dest/js',
+    bootstrap: "./node_modules/bootstrap/dist/js/bootstrap.min.js",
+    popper: "./node_modules/@popperjs/core/dist/umd/popper.min.js",
+    base: "../../contrib/bootstrap/js/base.js",
+    src: "./src/js/custom.js",
+    dest: "./dest/js",
   },
-  component : {
-    src: './components/**/src/*.scss',
-    watch: './components/**/src/*.scss',
-    dest: './components/',
-  }
+  component: {
+    src: "./components/**/src/*.scss",
+    watch: "./components/**/src/*.scss",
+    dest: "./components/",
+  },
 };
 
 function component() {
   return gulp
     .src(paths.component.src)
-    .pipe(sass().on('error', sass.logError))
-    .pipe(rename(function(path) {
-      path.dirname = path.dirname.replace('/src', '');
-    }))
+    .pipe(sass().on("error", sass.logError))
+    .pipe(
+      rename(function (path) {
+        path.dirname = path.dirname.replace("/src", "");
+      }),
+    )
     .pipe(gulp.dest(paths.component.dest));
 }
 
@@ -66,38 +67,38 @@ function styles() {
   return gulp
     .src([paths.scss.bootstrap, paths.scss.src])
     .pipe(sourcemaps.init())
-    .pipe(cached('styles'))
+    .pipe(cached("styles"))
     .pipe(newer(paths.scss.dest))
     .pipe(
       sass({
         includePaths: [
-          './node_modules/bootstrap/scss',
-          '../../contrib/bootstrap/scss',
+          "./node_modules/bootstrap/scss",
+          "../../contrib/bootstrap/scss",
         ],
-      }).on('error', sass.logError)
+      }).on("error", sass.logError),
     )
     .pipe($.postcss(postcssProcessors))
     .pipe(
       postcss([
         autoprefixer({
           browsers: [
-            'Chrome >= 35',
-            'Firefox >= 38',
-            'Edge >= 12',
-            'Explorer >= 10',
-            'iOS >= 8',
-            'Safari >= 8',
-            'Android 2.3',
-            'Android >= 4',
-            'Opera >= 12',
+            "Chrome >= 35",
+            "Firefox >= 38",
+            "Edge >= 12",
+            "Explorer >= 10",
+            "iOS >= 8",
+            "Safari >= 8",
+            "Android 2.3",
+            "Android >= 4",
+            "Opera >= 12",
           ],
         }),
-      ])
+      ]),
     )
     .pipe(sourcemaps.write())
     .pipe(gulp.dest(paths.scss.dest))
     .pipe(cleanCss())
-    .pipe(rename({ suffix: '.min' }))
+    .pipe(rename({ suffix: ".min" }))
     .pipe(gulp.dest(paths.scss.dest))
     .pipe(browserSync.stream());
 }
@@ -114,11 +115,8 @@ function js() {
 function serve() {
   gulp
     .watch([paths.scss.watch, paths.scss.bootstrap], styles)
-    .on('change', browserSync.reload);
-  gulp
-    .watch(paths.component.watch, component)
-    .on('change', browserSync.reload);
-
+    .on("change", browserSync.reload);
+  gulp.watch(paths.component.watch, component).on("change", browserSync.reload);
 }
 
 const build = gulp.series(styles, gulp.parallel(js, serve, component));
--- web/themes/custom/ts_theme/src/js/custom.js
+++ web/themes/custom/ts_theme/src/js/custom.js
@@ -3,16 +3,12 @@
  * Global utilities.
  *
  */
-(function($, Drupal) {
-
-  'use strict';
+(function ($, Drupal) {
+  "use strict";
 
   Drupal.behaviors.bootstrap_sass = {
-    attach: function(context, settings) {
-
+    attach: function (context, settings) {
       // Custom code here
-
-    }
+    },
   };
-
 })(jQuery, Drupal);
