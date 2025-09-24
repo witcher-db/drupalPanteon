6aded7d692616a6ac316891fa3475c094ffe018c
witcher <o.polishchuk@dev-branch.com>
2025-09-24 12:26:13 +0300

    refs #9684: Fix Drupal behavior, set up component properly, add breakpoints and disable loop on mobile

--- web/themes/custom/ts_theme/components/quote_carousel/quote_carousel.js
+++ web/themes/custom/ts_theme/components/quote_carousel/quote_carousel.js
@@ -1,21 +1,31 @@
-(function (Drupal) {
+(function (Drupal, once) {
   Drupal.behaviors.tsSwiper = {
     attach: function (context, settings) {
-      new Swiper('.QuoteLayoutCarousel', {
-        loop: true,
-        navigation: {
-          nextEl: '.swiper-button-next',
-          prevEl: '.swiper-button-prev',
-        },
+      once('tsSwiper', '.js-quote-carousel', context).forEach(function (el) {
+        if (!(el instanceof HTMLElement)) return;
+
+        new Swiper(el, {
+          loop: false,
+          navigation: {
+            nextEl: el.querySelector('.swiper-button-next'),
+            prevEl: el.querySelector('.swiper-button-prev'),
+          },
+          breakpoints: {
+            650: {
+              loop: true,
+            },
+          },
+        });
       });
     }
   };
-})(Drupal);
-(function (Drupal) {
+
   Drupal.behaviors.starsField = {
-    attach: function (context) {
-      once('starsField', '.field--name-field-quantity-of-stars', context).forEach((el) => {
-        const number = parseInt(el.textContent.trim(), 10);
+    attach: function (context, settings) {
+      once('stars-field', '.field--name-field-quantity-of-stars', context).forEach(function (el) {
+        if (!(el instanceof HTMLElement)) return;
+
+        let number = parseInt(el.textContent.trim(), 10);
 
         if (!isNaN(number) && number > 0) {
           el.textContent = '★'.repeat(number);
@@ -25,4 +35,5 @@
       });
     }
   };
-})(Drupal);
+
+})(Drupal, once);
